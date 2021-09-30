bdc44a46c8d7eaa509e7c5e3e15e7386f66ee482
p22tpearson <p22tpearson@fairfieldprep.org>
2021-09-30 11:08:01 -0400

    add file

---
+++ lab_3-1.py
@@ -0,0 +1,14 @@
+# author: cjp (amdg) 9/29/2021
+
+x= int(input("how many points did the team score?"))
+
+elif x>= 15:
+     print("they recieved the gold medal!")
+
+     elif x>= 12:
+          print ("they recieved the silver medal!")
+
+     elif x <9:
+             print("no medal for you")
+        else:
+             print(:"they recieved bronze!")
